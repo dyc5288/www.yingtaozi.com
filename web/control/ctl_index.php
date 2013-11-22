5062b11f4ad31e90a8ed3cfe8b6dac0230822d03
duanyunchao <duanyunchao@115.com>
2013-11-23 00:05:31 +0800

    小丸子

--- web/control/ctl_index.php
+++ web/control/ctl_index.php
@@ -13,34 +13,9 @@ class ctl_index
 
     public function index()
     {
-        //phpinfo();die;
-        //echo "test";
-        $return = array();
-        try
-        {
-            T(10000);
-        }
-        catch (Exception $e)
-        {
-            ET($e->getMessage(), $return);
-            $return['message'] = $e->getMessage();
-        }
-        
-        pub_mod_test::add();
-        pub_mod_test::update();
-        var_dump(pub_mod_test::get());
-        var_dump(pub_mod_test::get_list());
-        var_dump(pub_mod_test::get_count());
-        
-        lib_gearman::add_job($GLOBALS['CONFIG']['gearman'], 'MYTEST', $return, 3);
-        $result = SM('test3', 'D_100', '1');
-        var_dump($result);
-        $result = GM('D_100', '1');
-        var_dump($result);
-
-        //json_print($return);
+        $return = array();        
         lib_template::assign('return', $return);
-        lib_template::display('test.tpl');
+        lib_template::display('index.tpl');
     }
 
 }
