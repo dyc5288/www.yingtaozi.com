5fe0924fa801cd278af6b4b90bfd382a1a0dfb63
duanyunchao <duanyunchao@115.com>
2013-11-23 09:31:00 +0800

    小丸子

--- web/static/js/core.js
+++ web/static/js/core.js
@@ -2,5 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
+$(function(){
+    $(".jNav li").mouseenter(function(){
+        $(".jNav li").toggleClass("page_item");
+        $(this).toggleClass("current_page_item");
+    }).mouseleave(function(){
+        $(".jNav li").toggleClass("page_item");
+        $(this).toggleClass("current_page_item");
+    });
+});
