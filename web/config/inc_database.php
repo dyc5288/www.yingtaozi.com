dbbb8bb0b1d25add769758049b28bea592e9f262
duanyunchao <duanyunchao@115.com>
2013-11-23 18:22:29 +0800

    更新

--- web/config/inc_database.php
+++ web/config/inc_database.php
@@ -9,9 +9,9 @@
 $GLOBALS["DATABASE"] = array();
 
 /* my数据库 */
-$GLOBALS['DATABASE']['databases']['db_user']    = 'test';
-$GLOBALS['DATABASE']['databases']['db_pass']    = 'test';
-$GLOBALS['DATABASE']['databases']['db_name']    = 'test';
+$GLOBALS['DATABASE']['databases']['db_user']    = 'yingtaozi';
+$GLOBALS['DATABASE']['databases']['db_pass']    = 'yingtaozi';
+$GLOBALS['DATABASE']['databases']['db_name']    = 'yingtaozi';
 $GLOBALS['DATABASE']['databases']['db_charset'] = 'utf-8';
 
 $GLOBALS['DATABASE']['section'] = array();
@@ -19,8 +19,8 @@ $GLOBALS['DATABASE']['section'][0] = array();
 $GLOBALS['DATABASE']['section'][0]['table_name'] = array();
 $GLOBALS['DATABASE']['section'][0]["table_range"] = 1;
 $GLOBALS['DATABASE']['section'][0]["group_count"] = 1;
-$GLOBALS['DATABASE']['section'][0]["ips"][0]["master"] = array("db_host" => "127.0.0.1:3306", "db_name" => "test");  // 默认组
-$GLOBALS['DATABASE']['section'][0]["ips"][0]["slave"][] = array("db_host" => "127.0.0.1:3306", "db_name" => "test"); // 默认组
+$GLOBALS['DATABASE']['section'][0]["ips"][0]["master"] = array("db_host" => "127.0.0.1:3306", "db_name" => "yingtaozi");  // 默认组
+$GLOBALS['DATABASE']['section'][0]["ips"][0]["slave"][] = array("db_host" => "127.0.0.1:3306", "db_name" => "yingtaozi"); // 默认组
 
 /* Memcached Keys */
 $GLOBALS["DATABASE"]['MEMCACHED_PREFIX'] = array();
