23c0da548a112f08e260c3b440dbe78794a4fb3b
duanyunchao <duanyunchao@115.com>
2013-11-23 10:04:57 +0800

    小丸子

---
+++ web/control/ctl_video.php
@@ -0,0 +1,40 @@
+<?php
+
+!defined('IN_INIT') && exit('Access Denied');
+
+require_once 'ctl_parent.php';
+
+/**
+ * TV放送局
+ *
+ * @author duanyunchao
+ * @version $Id$
+ */
+class ctl_video extends ctl_parent
+{
+
+    /**
+     * 初始化
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 首页
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        $return = array('nav' => 'video');
+        lib_template::assign('return', $return);
+        lib_template::display('info_index.tpl');
+    }
+
+}
+
+?>
