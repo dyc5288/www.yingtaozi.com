71189ae9502401d8974304ea2a5100a7c5bd67b4
duanyunchao <duanyunchao@115.com>
2013-11-23 09:52:04 +0800

    小丸子

---
+++ web/control/ctl_info.php
@@ -0,0 +1,40 @@
+<?php
+
+!defined('IN_INIT') && exit('Access Denied');
+
+require_once 'ctl_parent.php';
+
+/**
+ * 情报站
+ *
+ * @author duanyunchao
+ * @version $Id$
+ */
+class ctl_info extends ctl_parent
+{
+
+    /**
+     * 初始化
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 首页
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        $return = array('nav' => 'info');
+        lib_template::assign('return', $return);
+        lib_template::display('info_index.tpl');
+    }
+
+}
+
+?>
