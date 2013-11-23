bdf6d47fd29e16dd6f0a84987ab20f880f34d35c
duanyunchao <duanyunchao@115.com>
2013-11-23 10:04:43 +0800

    小丸子

--- web/static/js/core.js
+++ web/static/js/core.js
@@ -4,10 +4,18 @@
  */
 $(function(){
     $(".jNav li").mouseenter(function(){
+        $(".class_cur").toggleClass("current_page_item");
         $(".jNav li").toggleClass("page_item");
         $(this).toggleClass("current_page_item");
+        return false;
     }).mouseleave(function(){
-        $(".jNav li").toggleClass("page_item");
         $(this).toggleClass("current_page_item");
+        $(".jNav li").toggleClass("page_item");
+        $(".class_cur").toggleClass("current_page_item");
+        return false;
+    }).click(function(){
+        var url = $(this).find('a').attr('href');
+        window.location.href=url;
+        return false;
     });
 });
