8b3f2c0a3c92cafd0f6332d421b7cb30e54f6090
duanyunchao <duanyunchao@115.com>
2013-11-23 11:01:15 +0800

    小丸子

---
+++ web/control/ctl_fish.php
@@ -0,0 +1,40 @@
+<?php
+
+!defined('IN_INIT') && exit('Access Denied');
+
+require_once 'ctl_parent.php';
+
+/**
+ * 掏周边
+ *
+ * @author duanyunchao
+ * @version $Id$
+ */
+class ctl_fish extends ctl_parent
+{
+
+    /**
+     * 初始化
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 首页
+     * 
+     * @return void
+     */
+    public function index()
+    {
+        $return = array('nav' => 'fish');
+        lib_template::assign('return', $return);
+        lib_template::display('info_index.tpl');
+    }
+
+}
+
+?>
