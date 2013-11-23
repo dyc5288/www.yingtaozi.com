74f3ff47d6744835bed54a7f8ee85901c048f1b1
duanyunchao <duanyunchao@115.com>
2013-11-23 09:52:56 +0800

    小丸子

---
+++ web/control/ctl_parent.php
@@ -0,0 +1,24 @@
+<?php
+
+!defined('IN_INIT') && exit('Access Denied');
+
+/**
+ * 控制器父类
+ *
+ * @author duanyunchao
+ * @version $Id$
+ */
+class ctl_parent
+{
+    /**
+     * 初始化
+     * 
+     * @return void
+     */
+    protected function __construct()
+    {
+        
+    }
+}
+
+?>
