4be2e6f7412d292813347ee1d333819e203b708b
duanyunchao <duanyunchao@115.com>
2013-11-29 22:30:02 +0800

    用户表

--- web/phpscript/install.php
+++ web/phpscript/install.php
@@ -21,16 +21,16 @@ if (!empty($flag['ytz_users']))
 {
     $number = 1;
     $sql    = "CREATE TABLE IF NOT EXISTS `%s` (
-            `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-            `user_login` varchar(60) NOT NULL DEFAULT '',
-            `user_pass` varchar(64) NOT NULL DEFAULT '',
-            `user_nicename` varchar(50) NOT NULL DEFAULT '',
-            `user_email` varchar(100) NOT NULL DEFAULT '',
-            `user_url` varchar(100) NOT NULL DEFAULT '',
-            `user_registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+            `user_login` varchar(60) NOT NULL DEFAULT '' COMMENT '用户名',
+            `user_pass` varchar(64) NOT NULL DEFAULT '' COMMENT '密码',
+            `user_nicename` varchar(50) NOT NULL DEFAULT '' COMMENT '昵称',
+            `user_email` varchar(100) NOT NULL DEFAULT '' COMMENT '电子邮件',
+            `user_url` varchar(100) NOT NULL DEFAULT '' COMMENT '站点地址',
+            `user_registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '添加时间',
             `user_activation_key` varchar(60) NOT NULL DEFAULT '',
-            `user_status` int(11) NOT NULL DEFAULT '0',
-            `display_name` varchar(250) NOT NULL DEFAULT '',
+            `user_status` int(11) NOT NULL DEFAULT '0' COMMENT '用户状态',
+            `display_name` varchar(250) NOT NULL DEFAULT '' COMMENT '公开显示名字',
             PRIMARY KEY (`ID`),
             KEY `user_login_key` (`user_login`),
             KEY `user_nicename` (`user_nicename`)
