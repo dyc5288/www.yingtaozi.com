c90f78bfa07a434612d4a6fd139bb07961ca2ed4
duanyunchao <duanyunchao@115.com>
2013-11-29 22:02:02 +0800

    用户表

--- web/phpscript/install.php
+++ web/phpscript/install.php
@@ -14,38 +14,35 @@ $flag = hlp_common::get_cmd_flag();
 
 if (!empty($flag['help']))
 {
-    echo "php install.php -ytz_posts 1" . PHP_EOL;
+    echo "php install.php -ytz_users 1 用户表" . PHP_EOL;
 }
 
-if (!empty($flag['ytz_posts']))
+if (!empty($flag['ytz_users']))
 {
     $number = 1;
     $sql    = "CREATE TABLE IF NOT EXISTS `%s` (
-        `pid` int(10) unsigned NOT NULL AUTO_INCREMENT,
-        `post_author` varchar(100) NOT NULL DEFAULT '',
-        `post_ctime` int(10) DEFAULT NULL DEFAULT 0 COMMENT '添加时间',
-        `post_content` longtext NOT NULL,
-        `post_title` text NOT NULL,
-        `post_excerpt` text NOT NULL,
-        `comment_status` tinyint(4) NOT NULL DEFAULT 0,
-        `post_name` varchar(200) NOT NULL DEFAULT '',
-        `post_utime` int(10) DEFAULT NULL DEFAULT 0 COMMENT '修改时间',
-        `post_type` tinyint(4) NOT NULL DEFAULT 0,
-        `post_mime_type` varchar(100) NOT NULL DEFAULT '',
-        `comment_count` bigint(20) NOT NULL DEFAULT '0',
-        PRIMARY KEY (`pid`),
-        KEY `post_name` (`post_name`),
-        KEY `post_type` (`post_type`),
-        KEY `post_author` (`post_author`)
-        ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8";
+            `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            `user_login` varchar(60) NOT NULL DEFAULT '',
+            `user_pass` varchar(64) NOT NULL DEFAULT '',
+            `user_nicename` varchar(50) NOT NULL DEFAULT '',
+            `user_email` varchar(100) NOT NULL DEFAULT '',
+            `user_url` varchar(100) NOT NULL DEFAULT '',
+            `user_registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            `user_activation_key` varchar(60) NOT NULL DEFAULT '',
+            `user_status` int(11) NOT NULL DEFAULT '0',
+            `display_name` varchar(250) NOT NULL DEFAULT '',
+            PRIMARY KEY (`ID`),
+            KEY `user_login_key` (`user_login`),
+            KEY `user_nicename` (`user_nicename`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8";
 
-    if ($flag['ytz_posts'] == 1)
+    if ($flag['ytz_users'] == 1)
     {
-        hlp_tool::create_table('ytz_posts', $sql, $number);
+        hlp_tool::create_table('ytz_users', $sql, $number);
     }
-    else if ($flag['ytz_posts'] == 2)
+    else if ($flag['ytz_users'] == 2)
     {
-        hlp_tool::drop_table('ytz_posts', $number);
+        hlp_tool::drop_table('ytz_users', $number);
     }
 }
 
