79b0ebab16893f2f088785d8bde1622f84e91a82
duanyunchao <duanyunchao@115.com>
2013-11-30 00:30:40 +0800

    文章表

--- web/phpscript/install.php
+++ web/phpscript/install.php
@@ -17,6 +17,7 @@ if (!empty($flag['help']))
     echo "php install.php -ytz_users 1 用户表" . PHP_EOL;
     echo "php install.php -ytz_usermeta 1 用户额外参数表" . PHP_EOL;
     echo "php install.php -ytz_options 1 网站设置选项表" . PHP_EOL;
+    echo "php install.php -ytz_posts 1 文章表" . PHP_EOL;
 }
 
 if (!empty($flag['ytz_options']))
@@ -115,6 +116,50 @@ if (!empty($flag['ytz_options']))
     else if ($flag['ytz_options'] == 2)
     {
         hlp_tool::drop_table('ytz_options', $number);
+    }
+}
+
+if (!empty($flag['ytz_posts']))
+{
+    $number = 1;
+    $sql    = "CREATE TABLE IF NOT EXISTS `%s` (
+            `ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+            `post_author` bigint(20) unsigned NOT NULL DEFAULT '0',
+            `post_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            `post_date_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            `post_content` longtext NOT NULL,
+            `post_title` text NOT NULL,
+            `post_excerpt` text NOT NULL,
+            `post_status` varchar(20) NOT NULL DEFAULT 'publish',
+            `comment_status` varchar(20) NOT NULL DEFAULT 'open',
+            `ping_status` varchar(20) NOT NULL DEFAULT 'open',
+            `post_password` varchar(20) NOT NULL DEFAULT '',
+            `post_name` varchar(200) NOT NULL DEFAULT '',
+            `to_ping` text NOT NULL,
+            `pinged` text NOT NULL,
+            `post_modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            `post_modified_gmt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+            `post_content_filtered` longtext NOT NULL,
+            `post_parent` bigint(20) unsigned NOT NULL DEFAULT '0',
+            `guid` varchar(255) NOT NULL DEFAULT '',
+            `menu_order` int(11) NOT NULL DEFAULT '0',
+            `post_type` varchar(20) NOT NULL DEFAULT 'post',
+            `post_mime_type` varchar(100) NOT NULL DEFAULT '',
+            `comment_count` bigint(20) NOT NULL DEFAULT '0',
+            PRIMARY KEY (`ID`),
+            KEY `post_name` (`post_name`),
+            KEY `type_status_date` (`post_type`,`post_status`,`post_date`,`ID`),
+            KEY `post_parent` (`post_parent`),
+            KEY `post_author` (`post_author`)
+            ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8";
+    
+    if ($flag['ytz_posts'] == 1)
+    {
+        hlp_tool::create_table('ytz_posts', $sql, $number);
+    }
+    else if ($flag['ytz_posts'] == 2)
+    {
+        hlp_tool::drop_table('ytz_posts', $number);
     }
 }
 
