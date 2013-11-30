282ce633329fd3601c4d3b5853bef8c63b69c578
duanyunchao <duanyunchao@115.com>
2013-11-30 19:48:26 +0800

    地址

---
+++ web/library/smarty/plugins/modifier.url.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Smarty plugin
+ * 
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * Smarty url modifier plugin
+ *
+ * Type:     modifier<br>
+ * Name:     url<br>
+ * Purpose:  Truncate a string to a certain length if necessary,
+ *           optionally splitting in the middle of a word, and
+ *           appending the $etc string or inserting $etc into the middle.
+ * @link http://smarty.php.net/manual/en/language.modifier.url.php
+ *          url (Smarty online manual)
+ * @author   Monte Ohrt <monte at ohrt dot com>
+ * @param string
+ * @param integer
+ * @param string
+ * @param boolean
+ * @param boolean
+ * @return string
+ */
+function smarty_modifier_url($data, $type = false)
+{
+    $url = '';
+    
+    switch ($type)
+    {
+        case 'info_detail':
+            $url = URL . "/?c=info&a=detail&id={$data}";
+            break;
+        default:
+            $url = URL;
+            break;
+    }
+    
+    return $url;
+}
+
+/* vim: set expandtab: */
+
