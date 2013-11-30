a222d4473fe77f5668aa18047c6e270a6ad37f4d
duanyunchao <duanyunchao@115.com>
2013-12-01 01:44:19 +0800

    情报站

--- web/control/ctl_parent.php
+++ web/control/ctl_parent.php
@@ -19,6 +19,21 @@ class ctl_parent
     {
         
     }
+
+    /**
+     * 热点资讯
+     * 
+     * @return void
+     */
+    protected function _hot(&$return)
+    {
+        $order = 'comment_count desc';
+        $cond = array();
+        $cond['post_status'] = pub_mod_posts::STATUS_PUBLISH;
+        $cond['post_type']   = pub_mod_posts::TYPE_POST;
+        $return['hot']       = pub_mod_posts::get_list($cond, $order, 0, 5, pub_mod_posts::COLUMN_INFO_HOT);
+    }
+
 }
 
 ?>
