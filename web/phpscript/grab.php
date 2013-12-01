385866db2da83898a18f8274799039c633295236
duanyunchao <duanyunchao@115.com>
2013-12-01 14:17:20 +0800

    抓取资讯

---
+++ web/phpscript/grab.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * 抓取
+ * 
+ * @author duanyunchao
+ * @version $Id$
+ */
+/** 初始化 */
+require '../init.php';
+
+/* 调试模式 */
+$flag = hlp_common::get_cmd_flag();
+
+if (!empty($flag['help']))
+{
+    echo "php grab.php -grab_info 1 抓取恶魔岛资讯" . PHP_EOL;
+}
+
+if (!empty($flag['grab_info']))
+{
+    switch ($flag['grab_info'])
+    {
+        case 1:
+            for ($i = 1; $i <= 776; $i++)
+            {
+                $params = array('url'  => "http://news.emland.net/index.php?&page={$i}", 'type' => $flag['grab_info']);
+                lib_gearman::add_job($GLOBALS['CONFIG']['gearman'], 'GRAB_INFO', $params, 3);
+            }
+            break;
+    }
+}
+
+echo "success" . PHP_EOL;
