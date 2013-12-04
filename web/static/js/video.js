59d46cf12719d7b70fd8e0ac397dad710c6b4f6a
duanyunchao <duanyunchao@115.com>
2013-12-05 00:01:35 +0800

    七牛接口

---
+++ web/static/js/video.js
@@ -0,0 +1,14 @@
+$(function(){
+    var fbutton = $(".jOrder").find("div");
+    fbutton.click(function(){
+        var url = $(this).attr('url');
+        var play = $("#js_play");
+        var cur_url = play.attr('src');
+        if (url != cur_url) {
+            $(".jOrder").find(".jCurrent").toggleClass("jCurrent");
+            $(this).toggleClass("jCurrent");
+            play.attr('src', url);
+        }
+        return false;
+    });
+});
