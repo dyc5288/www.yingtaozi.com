c0414673a58f08ce6bba013e0c61cc39bc2a30a5
duanyunchao <duanyunchao@115.com>
2013-12-12 01:21:13 +0800

    涛周边

--- web/static/js/fish.js
+++ web/static/js/fish.js
@@ -5,6 +5,15 @@ $(function(){
         window.location.href = '/?c=fish&keyword='+keyword;
         return false;
     });
+    var cur_keyword=$("#js_dcatea").attr('cur_keyword');
+    cateas.each(function(){
+        var k = $(this).html();
+        if (k == cur_keyword) {
+            $(this).addClass('jCurrent');
+            return false;
+        }
+        return false;
+    });
     var catebs = $("#js_dcateb").find("div");
     catebs.click(function(){
         var keyword = $(this).html();
